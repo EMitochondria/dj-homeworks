48d43aa182a779ad677722a52901fb80c92ac0e4
Gamil Khakimov <Gamil.Khakimov@icl-services.com>
2022-12-27 17:13:15 +0300

    add inter table and change admin

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from .models import Article, Scope, Tag
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):       
+    def clean(self):
+        COUNTER = 0
+        for form in self.forms:
+            data = form.cleaned_data.get('is_main')
+            if data==True:                
+                COUNTER += 1      
+        if COUNTER != 1:
+            raise ValidationError('Тут всегда ошибка')
+        return super().clean()
+
+    
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display=['name',]
