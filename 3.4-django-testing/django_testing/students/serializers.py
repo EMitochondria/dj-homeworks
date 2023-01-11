f9f4eea92fe88082d6bc8bba4a6d6355d7ddcc73
Gamil Khakimov <Gamil.Khakimov@icl-services.com>
2023-01-11 13:33:31 +0300

    done musthave part of hw

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from students.models import Course
+from django_testing.settings import MAX_STUDENTS_PER_COURSE
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -8,3 +9,11 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate(self, data):
+        """Проверка, что на курсе не больше больше 20 студентов"""
+        students = data['students']
+        students_count = len(students)
+        if students_count >= MAX_STUDENTS_PER_COURSE:
+            raise ValidationError('Вы не можете добавить больше 20 студентов в курс')
+        return data
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,117 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from students.models import Course, Student
+from model_bakery import baker
+from rest_framework import status
+from django.urls import reverse
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return  baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return  baker.make(Course, *args, **kwargs)
+    return factory
+
+@pytest.mark.django_db
+def test_api_course_one(client, course_factory):
+    """Проверка получения 1го курса"""
+    courses = course_factory(_quantity=1)
+    response = client.get(f'/api/v1/courses/{courses[0].id}/')
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data['name'] == courses[0].name
+
+@pytest.mark.django_db
+def test_api_courses_list(client, course_factory):
+    """Проверка получения списка курсов"""
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')    
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert len(data) == len(courses)
+    for i, course in enumerate(data):
+        assert course['name'] == courses[i].name
+
+@pytest.mark.django_db
+def test_api_course_filter_by_id(client, course_factory):
+    """Проверка фильтрации списка курсов по id"""
+    courses = course_factory(_quantity=10)    
+    id = courses[0].id
+    response =  client.get(reverse('courses-list'), kwargs = {'id': id})
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    for i, course in enumerate(data):
+        assert course['id'] == courses[i].id
+
+@pytest.mark.django_db
+def test_api_course_filter_by_name(client, course_factory):
+    """Проверка фильтрации списка курсов по name"""
+    courses = course_factory(_quantity=10)  
+    response =  client.get(reverse('courses-list'), kwargs= {'name': courses[1].name})
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    for i, course in enumerate(data):
+        assert course['name'] == courses[i].name
+
+@pytest.mark.django_db
+def test_create_course(client, student_factory):
+    """Проверка успешного создания курса"""
+    count_courses = Course.objects.count()
+    students = student_factory(_quantity=1)
+    list_students_id = []
+    for student in students:
+        list_students_id.append(student.id)
+    response =  client.post('/api/v1/courses/', data = {'name': 'test', 'students': list_students_id})
+    assert response.status_code == status.HTTP_201_CREATED
+    assert count_courses+1 == Course.objects.count()
+
+@pytest.mark.django_db
+def test_courses_update(client, course_factory, student_factory):
+    """Проверка успешного обновления курса"""    
+    courses = course_factory(_quantity=1)    
+    students = student_factory(_quantity=1)
+    list_students_id = []
+    for student in students:
+        list_students_id.append(student.id)
+    new_name = 'modified'
+    data = {
+        'name': new_name,
+        'students': list_students_id
+    }
+    for course in courses:
+        response = client.patch(reverse('courses-detail', args=[course.id]), data = data)
+        assert response.status_code == status.HTTP_200_OK
+        data = response.json()
+        assert data['name'] == new_name
+
+@pytest.mark.django_db
+def test_courses_delete(client, course_factory):
+    """Проверка успешного удаления курса"""    
+    courses = course_factory(_quantity=1)
+    for course in courses:
+        response = client.delete(reverse('courses-detail', args=[course.id]))
+        print(response.status_code)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+   
+@pytest.mark.django_db
+def test_max_student_count_on_course(client, course_factory, student_factory):
+    """Проверка на максимальное число студентов на одном курсе 20 c student_factory"""    
+    courses = course_factory(_quantity=1)
+    students = student_factory(_quantity=20)
+    list_students_id = []
+    for student in students:
+        list_students_id.append(student.id)
+    for course in courses:
+        response = client.patch(reverse('courses-detail', args=[course.id]), {'name': course.name, "students": list_students_id})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST  
